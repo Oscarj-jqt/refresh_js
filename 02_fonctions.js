3b641efb2d068d1a989c6b5061eb074f3d521a08
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-14 15:05:40 +0200

    last commit

--- 02_fonctions.js
+++ 02_fonctions.js
@@ -14,3 +14,22 @@ function direBonjour(){
 
 //setTimout(callback, temps)
 setTimeout(direBonjour,3000);
+
+const maStruct = {
+    a:2,
+    ajouter2AA:() => { this.a +=2;},
+    ajouter2AAbis: function() {this.a += 2;}
+};
+
+console.log(maStruct.a)//affiche 2
+maStruct.ajouter2AA();
+console.log(maStruct.a)//affiche 2, ca ne marche pas avec la fonction anonyme fléchée
+maStruct.ajouter2AAbis();
+console.log(maStruct.a) // affiche 4
+
+
+
+
+
+
+
