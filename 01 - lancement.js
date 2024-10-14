650175f0165eb2336d01cefc583d17ce765906c5
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-14 14:24:14 +0200

    rappel fonction

---
+++ 01 - lancement.js
@@ -0,0 +1,8 @@
+//aucune indication sur la portée de a (il peut être modifié, avoir sa valeur différente dans une fonction, condition...)
+a = 1;
+//b est égale à 2 à portée globale, dans plusieurs cadres(condition, fonction et en dehors)
+var b = 2;
+//c est égale à 3 dans un cadre précis (condition, fonction...)
+let c = 3;
+//d est égale à 4 et ne varie pas
+const d = 4;
---
+++ 02 - les fonctions.js
@@ -0,0 +1,8 @@
+function maFonction(param1, param2) {
+    return param1 + param2
+}
+
+console.log(maFonction(1,2))//affiche 3
+console.log(maFonction(2))//affiche NaN
+console.log(maFonction('a','b'))//affiche ab
+
