946462bd5bdbbc4e31ede5c4ed8485d21a4547f6
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-10-14 14:48:26 +0200

    last commit

--- 02_fonctions.js
+++ 02_fonctions.js
@@ -5,4 +5,12 @@ function maFonction(param1, param2) {
 console.log(maFonction(1,2))//affiche 3
 console.log(maFonction(2))//affiche NaN
 console.log(maFonction('a','b'))//affiche ab
+console.log(maFonction(undefined,2))//affiche NaN
 
+
+function direBonjour(){
+    return 'Bonjour'
+}
+
+//setTimout(callback, temps)
+setTimeout(direBonjour,3000);
